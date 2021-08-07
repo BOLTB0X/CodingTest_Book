0e01ad49d2f0259d5690e2c2ba5310127572481a
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-08-07 13:21:39 +0900

    5 - 11 미로탈출

--- 05DFS&BFS/5 - 11 미로탈출.cpp
+++ 05DFS&BFS/5 - 11 미로탈출.cpp
@@ -2,38 +2,48 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-int n, m;
-int graph[201][201];
-int dx[4] = { -1, 1, 0, 0 };
-int dy[4] = { 0, 0, -1, 1 };
 
-int bfs(int x, int y) {
-    queue<pair<int, int> > q;
-    q.push({ x, y });
-    while (!q.empty()) {
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            if (graph[nx][ny] == 0) continue;
-            if (graph[nx][ny] == 1) {
-                graph[nx][ny] = graph[x][y] + 1;
-                q.push({ nx, ny });
-            }
-        }
-    }
-    return graph[n - 1][m - 1];
+int n, m;
+int map[201][201];
+
+int dy[4] = { -1,0,1,0 };
+int dx[4] = { 0,1,0,-1 };
+
+int BFS(int y, int x) {
+	queue<pair<int, int>> que;
+	que.push({ y,x });
+
+	while (!que.empty()) {
+		int cur_y = que.front().first;
+		int cur_x = que.front().second;
+		que.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int ny = cur_y + dy[i];
+			int nx = cur_x + dx[i];
+
+			if (ny < 0 || ny >= n || nx<0 || nx>=m) 
+				continue;
+			if (map[ny][nx] == 0) 
+				continue;
+			if (map[ny][nx] == 1) {
+				map[ny][nx] = map[cur_y][cur_x] + 1;
+				que.push({ ny,nx });
+			}
+		}
+	}
+	return map[n - 1][m - 1];
 }
+
 int main(void) {
-    cin >> n >> m;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            scanf("%1d", &graph[i][j]);
-        }
-    }
-    cout << bfs(0, 0) << endl;
-    return 0;
+	cin >> n >> m;
+
+	for (int y = 0; y < n; y++) {
+		for (int x = 0; x < n; x++) {
+			scanf("%1d", &map[y][x]);
+		}
+	}
+
+	cout << BFS(0, 0)<< '\n';
+	return 0;
 }
