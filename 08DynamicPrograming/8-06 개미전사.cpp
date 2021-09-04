282a87f683c46a4ba8778cec4106b777780d7227
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-04 19:51:44 +0900

    C++

--- 08DynamicPrograming/8-06 개미전사.cpp
+++ 08DynamicPrograming/8-06 개미전사.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-int n;
-int d[100] = {0};
-int arr[1001];
+int max(int a, int b) {
+	return a > b ? a : b;
+}
 
-int solution(int n,int* arr) {
-	d[0] = arr[0];
-	d[1] = max(arr[0], arr[1]);
-	for (int i = 2; i < n; i++) {
-		d[i] = max(d[i - 1], d[i - 2] + arr[i]);
+int solution(int arr[], int n) {
+	int answer;
+	int dp[101] = { 0, };
+
+	dp[1] = arr[1];
+	dp[2] = max(arr[1], arr[2]);
+	//보텀업
+	for (int i = 3; i <= n; i++) {
+		dp[i] = max(dp[i - 1], dp[i - 2] + arr[i]);
 	}
-	return d[n - 1];
+	answer = dp[n];
+	return answer;
 }
 
 int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int n;
 	cin >> n;
-	for (int i = 0; i < n; i++) {
+	int arr[101];
+	for (int i = 1; i <= n; i++)
 		cin >> arr[i];
-	}
-	cout << solution(n, arr) << '\n';
+	int ret = solution(arr, n);
+	cout << ret << '\n';
+	return 0;
 }
