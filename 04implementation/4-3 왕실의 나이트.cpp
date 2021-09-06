117cd03e02da097dad179e67dbe140e8aa661600
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-06 15:53:06 +0900

    왕실의 나이트

--- 04implementation/4-3 왕실의 나이트.cpp
+++ 04implementation/4-3 왕실의 나이트.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
+#include <string>
 using namespace std;
-string inputData;
 
-int dx[8] = { -2, -1, 1, 2, 2, 1, -1, -2 };
-int dy[8] = { -1, -2, -2, -1, 1, 2, 2, 1 };
+const int dr[8] = { -2, -1, 1, 2, 2, 1, -1, -2 };
+const int dc[8] = { -1, -2, -2, -1, 1, 2, 2, 1 };
+
+bool is_range(int row, int col) {
+	if (row < 1 || col < 1 || row > 8 || col > 8)
+		return false;
+	return true;
+}
 
 int main(void) {
-    cin >> inputData;
-    int row = inputData[1] - '0';
-    int column = inputData[0] - 'a' + 1;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-    int result = 0;
-    for (int i = 0; i < 8; i++) {
-        int next_Row = row + dx[i];
-        int next_Column = column + dy[i];
-        if (next_Row >= 1 && next_Row <= 8 && next_Column >= 1 && next_Column <= 8) {
-            result += 1;
-        }
-    }
-    cout << result << '\n';
-    return 0;
+	string input_str;
+	//문자형+정수
+	cin >> input_str;
+
+	int row = input_str[1] - '0';
+	int col = input_str[0] - 'a' + 1;
+
+	int result = 0;
+	for (int i = 0; i < 8; i++) {
+		int nr = row + dr[i];
+		int nc = col + dc[i];
+		if (is_range(nr, nc)) {
+			result++;
+		}
+	}
+	cout << result << '\n';
+	return 0;
 }
