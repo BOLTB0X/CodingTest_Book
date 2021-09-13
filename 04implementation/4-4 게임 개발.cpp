72fb0053fa8202d4e36afdc4e31b27aee842d11a
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-13 14:16:18 +0900

    게임개발

--- 04implementation/4-4 게임 개발.cpp
+++ 04implementation/4-4 게임 개발.cpp
@@ -2,69 +2,75 @@
 using namespace std;
 
 int n, m;
-int r, c, dir;
 int map[50][50];
-int dist[50][50];
+bool visited[50][50];
+int result = 1;
 
-int turn_left() {
-	dir -= 1;
-	if (dir == -1)
-		dir = 3;
-	return dir;
+//북동남서
+const int dx[4] = { -1,0,1,0 };
+const int dy[4] = { 0,1,0,-1 };
+
+//왼쪽으로 방향전환
+int turn_dir(int d) {
+	if (d == 0) 
+		return 3;
+	return d - 1;
 }
 
-int simulation() {
-	const int dr[4] = { -1,0,1,0 };
-	const int dc[4] = { 0,1,0,-1 };
-	
-	int cnt = 1;
+void simulation(int x, int y, int dir) {
 	int turn_cnt = 0;
+	visited[x][y] = true;
 	while (true) {
-		turn_left();
-		int nr = r + dr[dir];
-		int nc = c + dc[dir];
+		//왼쪽방향부터 이동가능한지
+		dir = turn_dir(dir);
+		int nx = x + dx[dir];
+		int ny = y + dy[dir];
 		
-		if (dist[nr][nc] == 0 && map[nr][nc] == 0) {
-			dist[nr][nc] = 1;
-			r = nr;
-			c = nc;
-			cnt++;
+		//만약 육지이고 미방문이면
+		if (map[nx][ny] == 0 && !visited[nx][ny]) {
+			visited[nx][ny] = true;
+			x = nx;
+			y = ny;
+			//이동 했으므로 
+			result++;
 			turn_cnt = 0;
 			continue;
 		}
-		else 
-			turn_cnt++;
+		//갈 수 없다면
+		else {
+			turn_cnt++; //다음방향 탐색
+		}
+		//만약 4방향 모두 갈수 없다면
 		if (turn_cnt == 4) {
-			nr = r - dr[dir];
-			nc = c - dc[dir];
-			if (map[nr][nc] == 0) {
-				r = nr;
-				c = nc;
+			int nx = x - dx[dir];
+			int ny = y - dy[dir];
+			//육지일 겨웅
+			if (map[nx][ny] == 0) {
+				x = nx; 
+				y = ny;
 			}
 			else
 				break;
 			turn_cnt = 0;
 		}
 	}
-	return cnt;
 }
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	//맵 생성
+	int c, r, dir;
 	
 	cin >> n >> m;
-	cin >> r >> c >> dir;
+	cin >> c >> r >> dir;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> map[i][j];
+	for (int x = 0; x < n; x++) {
+		for (int y = 0; y < m; y++) {
+			cin >> map[x][y];
 		}
 	}
-
-	int ret = simulation();
-	cout << ret << '\n';
+	simulation(c, r, dir);
+	cout << result << '\n';
 	return 0;
 }
