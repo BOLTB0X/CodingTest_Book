3ecb09728e10c7c9c92dd3a7d55cd1145006b458
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-10 00:24:46 +0900

    그리디

---
+++ 03Greedy/3 - 4 1이 될 때까지.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int k) {
+	int answer = 0;
+
+	while (1) {
+		//1이 될때까지
+		int target = n / k;
+
+		if (target == 0)
+			break;
+
+		answer += (n - (target * k));
+		n = target;
+		answer++;
+	}
+
+	answer += n - 1;
+
+	return answer;
+}
+int main(void) {
+	int n, k;
+
+	cin >> n >> k;
+
+	int ret = solution(n, k);
+	cout << ret;
+	return 0;
+}
