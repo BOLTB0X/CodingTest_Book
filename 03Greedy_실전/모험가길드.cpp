ba5ba1a401c734aa55bb6dd0b3727259b7d07e7d
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-10 00:25:15 +0900

    실전문제

--- 03Greedy_실전/모험가길드.cpp
+++ 03Greedy_실전/모험가길드.cpp
@@ -1,28 +1,78 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
+
+void merge(vector<int>& v, int start, int mid, int end) {
+	int* sorted = new int[end - start + 1];
+	int i, j, k;
+
+	i = start, j = mid + 1, k = 0;
+
+	while (i <= mid && j <= end) {
+		if (v[i] <= v[j])
+			sorted[k++] = v[i++];
+		else
+			sorted[k++] = v[j++];
+	}
+
+	if (i > mid)
+		while (j <= end) sorted[k++] = v[j++];
+	else
+		while (i <= mid) sorted[k++] = v[i++];
+
+	for (i = start, k = 0; i <= end; ++i, ++k)
+		v[i] = sorted[k];
+
+	delete[] sorted;
+	return;
+}
+
+void merge_Sort(vector<int>& v, int start, int end) {
+	if (start >= end)
+		return;
+
+	int mid = (start + end) / 2;
+	merge_Sort(v, start, mid);
+	merge_Sort(v, mid + 1, end);
+	merge(v, start, mid, end);
+
+	return;
+}
+
+int solution(int n, vector<int>& guild) {
+	int answer = 0;
+	int cnt = 0;
+
+	//오름차순 정렬
+	merge_Sort(guild, 0, n - 1);
+
+	//그리디 알고리즘
+	//모든 모험가를 넣을 필요 X
+	for (int& player : guild) {
+		cnt++; //인원수 카운트
+
+		//해당 플레이어 공포도 이상이면 ->  커버가능한 모험가이면
+		if (cnt >= player) {
+			answer++;
+			cnt = 0;
+		}
+	}
+	return answer;
+}
 
 int main() {
 	int n;
-	
-	int count = 0, answer = 0;
+	vector<int> guild;
 	
 	cin >> n;
-	vector<int> guild;
+	guild.resize(n, 0);
 
-	for (int i = 0; i < n; i++) {
-		int t;
-		cin >> t;
-		guild.push_back(t);
-	}
-	sort(guild.begin(), guild.end());
-	for (int i = 0; i < n; i++) {
-		count += 1;
-		if (count >= guild[i]) {
-			answer += 1;
-			count = 0;
-		}
-	}
-	cout << answer;
+	for (int i = 0; i < n; i++) 
+		cin >> guild[i];
+	
+	int ret = solution(n, guild);
+	cout << ret;
+
+	return 0;
 }
