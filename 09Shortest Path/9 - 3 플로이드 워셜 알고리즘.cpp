c3c27a0cd2bd41e2816b9eda25f9ffbe5bb771fd
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-11 13:44:14 +0900

    최단경로

--- 09Shortest Path/9 - 3 플로이드 워셜 알고리즘.cpp
+++ 09Shortest Path/9 - 3 플로이드 워셜 알고리즘.cpp
@@ -1,79 +1,69 @@
 #include <iostream>
-#define INF 1e9 // 무한을 의미하는 값으로 10억을 설정
+#include <vector>
+#define INF 1e9 //10억
 
 using namespace std;
 
-// 노드의 개수(N), 간선의 개수(M)
-// 노드의 개수는 최대 500개라고 가정
-int n, m;
-// 2차원 배열(그래프 표현)를 만들기
-int adj[501][501];
-
 int Min(int a, int b) {
-    return a < b ? a : b;
+	return a < b ? a : b;
 }
 
-void init(void) {
-    // 최단 거리 테이블을 모두 무한으로 초기화
-    for (int i = 0; i < 501; i++) {
-        for (int j = 0; j < 501; ++j)
-            adj[i][j] = INF;
-    }
-    
-    // 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
-    for (int a = 1; a <= n; a++) {
-        for (int b = 1; b <= n; b++) {
-            if (a == b) 
-                adj[a][b] = 0;
-        }
-    }
+//플로이드 
+void floyed(vector<vector<int>>& adj, int n) {
+	//점화식
+	for (int k = 1; k <= n; ++k) {
+		for (int a = 1; a <= n; ++a) {
+			for (int b = 1; b <= n; ++b)
+				adj[a][b] = Min(adj[a][b], adj[a][k] + adj[k][b]);
+		}
+	}
 
-    return;
+	return;
 }
 
-void solution(void) {
-    // 점화식에 따라 플로이드 워셜 알고리즘을 수행
-    for (int k = 1; k <= n; k++) {
-        for (int a = 1; a <= n; a++) {
-            for (int b = 1; b <= n; b++) 
-                adj[a][b] = Min(adj[a][b], adj[a][k] + adj[k][b]);
-        }
-    }
+int solution(vector<pair<int, int>> &edge, int n, int m, int x, int k) {
+	int answer = 0;
+	//1 - > k,  k -> x;
+	vector<vector<int>> adj(n + 1, vector<int>(n + 1, INF)); //인접행렬 INF로 초기화
 
-    //출력
-    // 수행된 결과를 출력
-    for (int a = 1; a <= n; a++) {
-        for (int b = 1; b <= n; b++) {
-            // 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
-            if (adj[a][b] == INF) {
-                cout << "INFINITY" << ' ';
-            }
-            // 도달할 수 있는 경우 거리를 출력
-            else {
-                cout << adj[a][b] << ' ';
-            }
-        }
-        cout << '\n';
-    }
+	//먼저 인접행렬로 자기 자신한테 가는 비용 초기화
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= n; ++j) {
+			if (i == j)
+				adj[i][j] = 0;
+		}
+	}
 
-    return;
+	//연결 여부 
+	for (pair<int, int>& e : edge) {
+		adj[e.first][e.second] = 1;
+		adj[e.second][e.first] = 1;
+	}
+
+	floyed(adj, n);
+
+	answer = (adj[1][k] + adj[k][x]) < INF ? (adj[1][k] + adj[k][x]) : -1;
+	return answer;
 }
 
 int main(void) {
-    cin >> n >> m;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-    init();
+	int n, m, x, k;
+	vector<pair<int, int>> edge;
+	cin >> n >> m;
 
-    // 각 간선에 대한 정보를 입력 받아, 그 값으로 초기화
-    for (int i = 0; i < m; i++) {
-        // A에서 B로 가는 비용은 C라고 설정
-        int a, b, c;
-        cin >> a >> b >> c;
-        adj[a][b] = c;
-    }
+	for (int i = 0; i < m; ++i) {
+		int a, b;
+		cin >> a >> b;
+		edge.push_back({ a,b });
+	}
 
-    //플로이드 워셜
-    solution();
+	cin >> x >> k;
 
-    return 0;
+	int ret = solution(edge, n, m, x, k);
+	cout << ret;
+	return 0;
 }
