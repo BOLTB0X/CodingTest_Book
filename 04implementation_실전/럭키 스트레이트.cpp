9ef601da9548478719dbc6cb20d4084e85efac9e
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-12 17:13:08 +0900

    럭키 스트레이트

--- 04implementation_실전/럭키 스트레이트.cpp
+++ 04implementation_실전/럭키 스트레이트.cpp
@@ -7,14 +7,21 @@ int main() {
 	int tot = 0;
 
 	cin >> st;
+	
+	//앞
 	for (int i = 0; i < st.length() / 2; i++) {
 		int num = st[i] - '0';
 		tot += num;
 	}
+	
+	//뒤
 	for (int i = st.length() / 2; i < st.length(); i++) {
 		int num = st[i] - '0';
 		tot -= num;
 	}
-	if (tot == 0) cout << "LUCKY" << endl;
-	else cout << "READY" << endl;
+	
+	if (tot == 0) 
+		cout << "LUCKY" << endl;
+	else 
+		cout << "READY" << endl;
 }
