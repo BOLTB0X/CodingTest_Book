311723424590bd1dda3036dd09fcfb019a936f52
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-14 17:00:46 +0900

    볼링공 고르기

--- 03Greedy_실전/볼링공 고르기.cpp
+++ 03Greedy_실전/볼링공 고르기.cpp
@@ -4,25 +4,29 @@ using namespace std;
 
 int solution(int n, int m, int* balls) {
 	int answer = 0;
-	int tmp = n; //임시
-	int cnt[11] = { 0, };
+	int* cnt = new int[m + 1];
+	for (int i = 0; i <= m; ++i)
+		cnt[i] = 0;
 
-	//번호별 카운트
-	for (int i = 0; i < n; ++i) 
+	for (int i = 0; i < n; ++i)
 		cnt[balls[i]]++;
-	
+
 	for (int i = 1; i <= m; ++i) {
-		tmp -= cnt[i];
-		answer += (tmp * cnt[i]);
+		//b가 선택할 경우의 수
+		n -= cnt[i];
+		//a가 선택할 경우의 수 -> cnt[i]
+		answer += (cnt[i] * n);
 	}
+
 	return answer;
 }
 
 int main(void) {
 	int n, m;
-	int balls[1001];
 
 	cin >> n >> m;
+	int* balls = new int[n];
+
 	for (int i = 0; i < n; ++i)
 		cin >> balls[i];
 
