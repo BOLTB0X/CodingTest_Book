e6baf27084f8b440f259bf87c342635b21a0e213
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-17 21:02:15 +0900

    1로 만들기

--- 08DynamicPrograming/8-05 1로 만들기.cpp
+++ 08DynamicPrograming/8-05 1로 만들기.cpp
@@ -1,20 +1,39 @@
 #include <iostream>
-#include <algorithm>
+
 using namespace std;
 
-long long d[30001];
+int Min(int a, int b) {
+	return a < b ? a : b;
+}
 
 int main(void) {
 	int n;
+
 	cin >> n;
-	for (int i = 2; i <= n; i++) {
-		d[i] = d[i - 1] + 1;
-		if (i % 2 == 0)
-			d[i] = min(d[i], d[i / 2] + 1);
-		if (i % 3 == 0)
-			d[i] = min(d[i], d[i / 3] + 1);
+	//dp 테이블 생성
+	int* dp = new int[n + 1];
+	for (int i = 0; i <= n; ++i)
+		dp[i] = 0;
+
+	// dp[1] = 0
+	// dp[2] = 1
+	// dp[3] = 1
+	// dp[4] = 2
+	// 보텀업 방식으로 
+	//즉 먼저 dp[i] = dp[i-1] + 1 ->  -1 
+	// 5,3,2는 해당 i가 나눠 떨어지는지 체크
+	for (int i = 2; i <= n; ++i) {
+		dp[i] = dp[i - 1] + 1; // -1
+		
 		if (i % 5 == 0)
-			d[i] = min(d[i], d[i / 5] + 1);
+			dp[i] = Min(dp[i / 5] + 1, dp[i]);
+		if (i % 3 == 0)
+			dp[i] = Min(dp[i / 3] + 1, dp[i]);
+		if (i % 2 == 0)
+			dp[i] = Min(dp[i / 2] + 1, dp[i]);
 	}
-	cout << d[n] << '\n';
+
+	cout << dp[n] << '\n';
+	free(dp); //메모리 해제
+	return 0;
 }
