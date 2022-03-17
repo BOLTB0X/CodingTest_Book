3382f3530ed966f0c7923a9edc7eb7568766f697
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-17 21:17:31 +0900

    개미 전사

---
+++ 08DynamicPrograming/8-06 개미 전사.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int Max(int a, int b) {
+	return a > b ? a : b;
+}
+
+int main(void) {
+	int n;
+
+	cin >> n;
+	int* arr = new int[n + 1];
+	int* dp = new int[n + 1];
+	dp[0] = 0;
+
+	for (int i = 1; i <= n; ++i) {
+		cin >> arr[i];
+		dp[i] = 0;
+	}
+
+	// 보텀업 방식
+	// 왼쪽부터 차례로 나아간다 생각하면
+	// 1번 -> 1, 2번에 왔을 때 2와 1중 큰 것
+	// 3부턴 dp테이블 참고하여 한칸 떨어진 것+ 현제와 바로 한칸 dp테이블
+
+	dp[0] = 0;
+	dp[1] = arr[1];
+	dp[2] = Max(arr[1], arr[2]);
+	//점화식
+	for (int i = 3; i <= n; ++i) 
+		dp[i] = Max(dp[i - 2] + arr[i], dp[i - 1]);
+	
+	cout << dp[n];
+	free(arr);
+	free(dp);
+	return 0;
+}
