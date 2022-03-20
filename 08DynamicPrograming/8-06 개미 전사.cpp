592139b4395aad6a6db0bc528bf0f729a288b60b
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-20 15:29:57 +0900

    개미 전사

--- 08DynamicPrograming/8-06 개미 전사.cpp
+++ 08DynamicPrograming/8-06 개미 전사.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// 최댓값 반환
 int Max(int a, int b) {
 	return a > b ? a : b;
 }
@@ -23,13 +24,11 @@ int main(void) {
 	// 왼쪽부터 차례로 나아간다 생각하면
 	// 1번 -> 1, 2번에 왔을 때 2와 1중 큰 것
 	// 3부턴 dp테이블 참고하여 한칸 떨어진 것+ 현제와 바로 한칸 dp테이블
-
-	dp[0] = 0;
 	dp[1] = arr[1];
 	dp[2] = Max(arr[1], arr[2]);
-	//점화식
+
 	for (int i = 3; i <= n; ++i) 
-		dp[i] = Max(dp[i - 2] + arr[i], dp[i - 1]);
+		dp[i] = Max(dp[i - 1], dp[i - 2] + arr[i]);
 	
 	cout << dp[n];
 	free(arr);
