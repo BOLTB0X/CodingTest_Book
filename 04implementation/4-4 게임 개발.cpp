35a07cca40f8f11f12e5d257a94c2320141cdf4a
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-20 15:16:08 +0900

    게임 개발

--- 04implementation/4-4 게임 개발.cpp
+++ 04implementation/4-4 게임 개발.cpp
@@ -1,76 +1,105 @@
 #include <iostream>
+
 using namespace std;
 
-int n, m;
-int map[50][50];
-bool visited[50][50];
-int result = 1;
+int board[51][51];
+int visited[51][51];
 
-//북동남서
-const int dx[4] = { -1,0,1,0 };
-const int dy[4] = { 0,1,0,-1 };
+// 북동남서
+const int dy[4] = { -1, 0, 1, 0 };
+const int dx[4] = { 0 , 1, 0, -1 };
 
-//왼쪽으로 방향전환
-int turn_dir(int d) {
-	if (d == 0) 
+// 초기화
+void init(int n, int m) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j)
+			visited[i][j] = 0;
+	}
+
+	return;
+}
+
+// 왼쪽으로 회전
+int turn_Left(int d) {
+	if (d == 0)
 		return 3;
 	return d - 1;
 }
 
-void simulation(int x, int y, int dir) {
-	int turn_cnt = 0;
-	visited[x][y] = true;
-	while (true) {
-		//왼쪽방향부터 이동가능한지
-		dir = turn_dir(dir);
-		int nx = x + dx[dir];
-		int ny = y + dy[dir];
-		
-		//만약 육지이고 미방문이면
-		if (map[nx][ny] == 0 && !visited[nx][ny]) {
-			visited[nx][ny] = true;
-			x = nx;
-			y = ny;
-			//이동 했으므로 
-			result++;
-			turn_cnt = 0;
-			continue;
-		}
-		//갈 수 없다면
-		else {
-			turn_cnt++; //다음방향 탐색
-		}
-		//만약 4방향 모두 갈수 없다면
-		if (turn_cnt == 4) {
-			int nx = x - dx[dir];
-			int ny = y - dy[dir];
-			//육지일 겨웅
-			if (map[nx][ny] == 0) {
-				x = nx; 
-				y = ny;
+// 범위
+int in_Range(int n, int m, int y, int x) {
+	return 0 <= y && y < n && 0 <= x && x < m;
+}
+
+// 솔루션
+int solution(int n, int m, int a, int b, int d) {
+	int answer = 1; // 현재 위치 고려
+	int y = a, x = b;
+	int play_cnt = 0;
+
+	// 방문리스트 초기화
+	init(n, m);
+
+	// 차례대로 시뮬레이션 시작
+	visited[y][x] = 1; // 방문 체크
+	while (1) {
+		d = turn_Left(d); // 회전
+		int ny = y + dy[d];
+		int nx = x + dx[d];
+
+		// 범위 내
+		if (in_Range(n, m, ny, nx) == 1) {
+			// 미방문이고 육지이면
+			if (visited[ny][nx] == 0 && board[ny][nx] == 0) {
+				visited[ny][nx] = 1;
+				y = ny, x = nx; // 변경
+				answer++; // 카운트
+				play_cnt = 0; // 초기화
+				continue; // 다시 1단계로
 			}
+
 			else
-				break;
-			turn_cnt = 0;
+				play_cnt++; // 다음방향 탐색
+
+			// 4방향 탐색후 갈 때가 없다면
+			if (play_cnt == 4) {
+				// 뒤로 돌아감
+				int ny = y - dy[d];
+				int nx = x - dx[d];
+
+				// 육지인 경우
+				if (board[ny][nx] == 0)
+					y = ny, x = nx;
+
+				// 바다인 경우 -> 멈춤
+				else
+					break;
+
+				play_cnt = 0; // 다시 초기화
+			}
 		}
+
+		else
+			continue;
 	}
+	
+	return answer;
 }
 
 int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	int c, r, dir;
-	
-	cin >> n >> m;
-	cin >> c >> r >> dir;
+	int n, m;
+	int a, b, d;
 
-	for (int x = 0; x < n; x++) {
-		for (int y = 0; y < m; y++) {
-			cin >> map[x][y];
-		}
+	cin >> n >> m;
+	cin >> a >> b >> d;
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j)
+			cin >> board[i][j];
 	}
-	simulation(c, r, dir);
-	cout << result << '\n';
+
+	int ret = solution(n, m, a, b, d);
+	cout << ret;
+
 	return 0;
 }
