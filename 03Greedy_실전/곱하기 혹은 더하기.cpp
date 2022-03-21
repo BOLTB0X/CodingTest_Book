3ec28adf0faf5c994aabd272d93c16b1e0983b07
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-21 14:15:50 +0900

    곱하기 혹은 더하기

--- 03Greedy_실전/곱하기 혹은 더하기.cpp
+++ 03Greedy_실전/곱하기 혹은 더하기.cpp
@@ -3,27 +3,30 @@
 
 using namespace std;
 
-long long solution(string str) {
-    long long answer = 0;
+long long solution(string numbers) {
+	long long answer = 0;
 
-    for (char& number : str) {
-        int n = number - '0';
+	// 문자열 차례로 확인
+	for (char& number : numbers) {
+		// 더하기
+		if (number - '0' <= 1 || answer <= 1)
+			answer += (number - '0');
 
-        if (n <= 1 || answer <= 1)
-            answer += n;
-        else
-            answer *= n;
-    }
+		// 곱하기
+		else
+			answer *= (number - '0');
+	}
 
-    return answer;
+	return answer;
 }
 
 int main(void) {
-    string str;
-    cin >> str;
-   
-    long long ret = solution(str);
-    cout << ret;
+	string numbers;
 
-    return 0;
+	cin >> numbers;
+
+	long long ret = solution(numbers);
+	cout << ret;
+
+	return 0;
 }
