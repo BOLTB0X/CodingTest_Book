30aa0d442798f9e0e24d9adbc4e1e7a9073dfc4a
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-21 20:34:07 +0900

    효율적인 화폐 구성

--- 08DynamicPrograming/8-08 효율적인 화폐 구성.cpp
+++ 08DynamicPrograming/8-08 효율적인 화폐 구성.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#define MV 10001 // 최댓값
+#define Max_Value 10001
 using namespace std;
 
 int coins[101];
-int dp[MV];
+int dp[Max_Value];
 
 // 최솟값 반환
 int Min(int a, int b) {
@@ -12,32 +12,30 @@ int Min(int a, int b) {
 
 int solution(int n, int m) {
 	int answer = -1;
-
 	// dp테이블 초기화
-	for (int i = 1; i <= m; ++i)
-		dp[i] = MV;
-	
 	dp[0] = 0;
-	// 보텀업
+	for (int i = 1; i <= m; ++i)
+		dp[i] = Max_Value;
+
+	// 보텀업방식
 	for (int i = 1; i <= n; ++i) {
 		int coin = coins[i]; // 사용할 동전
 		for (int j = 1; j <= m; ++j) {
-			if (dp[j - coin] >= 0) // 거슬러줄 수 있다면
+			if (j - coin >= 0)
 				dp[j] = Min(dp[j], dp[j - coin] + 1);
 		}
 	}
 
-	// 거슬러줄 수 있다면
-	if (dp[m] != MV)
-		answer = dp[m]; // 정답
-
+	// 거슬러 줄수 있다면
+	if (dp[m] != Max_Value)
+		answer = dp[m];
 	return answer;
 }
 
 int main(void) {
 	int n, m;
-
 	cin >> n >> m;
+
 	for (int i = 1; i <= n; ++i)
 		cin >> coins[i];
 
