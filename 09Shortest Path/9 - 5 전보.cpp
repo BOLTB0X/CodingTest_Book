ce6c385208b1fe39f57642d545780ea642dad72a
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-03 16:08:22 +0900

    전보

--- 09Shortest Path/9 - 5 전보.cpp
+++ 09Shortest Path/9 - 5 전보.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
-
-#define INF 1e9 // 무한을 의미하는 값으로 10억을 설정
+#include <queue>
+#define INF 1000000000 // 무한을 의미하는 값으로 10억을 설정
 #define MS 30001 //최대 길이
+#define LL long long
 
 using namespace std;
 
@@ -10,56 +11,34 @@ typedef struct {
     int target, cost;
 } Node;
 
-typedef struct {
-    int cur, cost;
-} Heap;
-
-Heap que[MS];
-int fr = 0, re = 0;
-
-void enqueue(Heap data) {
-    que[re++] = data;
-
-    if (fr < re) {
-        for (int i = fr + 1; i <= re; ++i) {
-            if (que[i].cost < que[i - 1].cost)
-                swap(que[i], que[i - 1]);
-        }
-    }
-    return;
-}
-
-Heap dequeue(void) {
-    return que[fr++];
-}
-
 vector<Node> graph[MS]; // 각 노드에 연결되어 있는 노드에 대한 정보를 담는 벡터
-int dist[MS]; // 최단 거리 테이블 만들기
+vector<int> dist; // 최단 거리 테이블 만들기
 
 //최댓값 반환
 int Max(int a, int b) {
     return a > b ? a : b;
 }
 
-void dijkstra(int start) {
-    enqueue({ start, 0 });
+void dijkstra(int n, int m, int start) {
+    priority_queue<pair<int,int>> pq; // 우선순위 큐 선언
+    pq.push({ 0,start });
     dist[start] = 0;
 
-    while (fr < re) {
-        Heap cur = dequeue();
+    while (!pq.empty()) {
+        int cur = pq.top().second;
+        int cdist = -pq.top().first;
+        pq.pop();
 
-        // 현재 노드가 이미 처리된 적이 있는 노드라면 무시
-        if (dist[cur.cur] < cur.cost)
+        // 이미 방문한 거리라면
+        if (dist[cur] < cdist)
             continue;
 
-        // 현재 노드와 연결된 다른 인접한 노드들을 확인
-        for (Node& next : graph[cur.cur]) {
-            int ncost = cur.cost + next.cost;
+        for (Node& next : graph[cur]) {
+            int ndist = cdist + next.cost;
 
-            // 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
-            if (ncost < dist[next.target]) {
-                dist[next.target] = ncost;
-                enqueue({ next.target, ncost });
+            if (ndist < dist[next.target]) {
+                dist[next.target] = ndist;
+                pq.push({ -ndist, next.target });
             }
         }
     }
@@ -69,14 +48,10 @@ void dijkstra(int start) {
 
 pair<int, int> solution(int n, int m, int start) {
     pair<int, int> answer;
-    for (int i = 1; i <= n; ++i)
-        dist[i] = INF;
+    int cnt = 0, max_dist = 0;
 
-    dijkstra(start); //다익스트라
-
-    int cnt = 0;
-    // 도달할 수 있는 노드 중에서, 가장 멀리 있는 노드와의 최단 거리
-    int max_dist = 0;
+    dist.resize(n + 1, INF);
+    dijkstra(n, m, start); // 다익스트라
 
     for (int i = 1; i <= n; ++i) {
         if (dist[i] != INF) {
@@ -84,7 +59,7 @@ pair<int, int> solution(int n, int m, int start) {
             max_dist = Max(max_dist, dist[i]);
         }
     }
-
+    
     answer.first = cnt - 1;
     answer.second = max_dist;
     return answer;
