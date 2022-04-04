1eec4c91245e27eb1c31927bf81060d235ecdf31
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-04 17:42:35 +0900

    연구소
    https://www.acmicpc.net/problem/14502

--- 05DFS&BFS_실전/연구소_DFS.cpp
+++ 05DFS&BFS_실전/연구소_DFS.cpp
@@ -4,52 +4,34 @@ using namespace std;
 
 int result = -1;
 int board[9][9];
-int tmp[9][9]; //백업용
-
 const int dy[4] = { 1,-1,0,0 };
-const int dx[4] = { 0,0,1,-1 };
+const int dx[4] = { 0,0,-1,1 };
 
-//최댓값 반환
-int Max(int a, int b) {
-	return a > b ? a : b;
-}
-
-//백업
-void backup(int n, int m) {
-	for (int i = 1; i <= n; ++i) {
-		for (int j = 1; j <= m; ++j)
-			tmp[i][j] = board[i][j];
-	}
-
-	return;
-}
-
+// 깊이우선탐색
 void DFS(int n, int m, int y, int x) {
 	for (int dir = 0; dir < 4; ++dir) {
 		int ny = y + dy[dir];
 		int nx = x + dx[dir];
 
-		//범위초과
-		if (ny < 1 || nx < 1 || ny > n || nx > m)
-			continue;
-		
-		//벽이거나 바이러스
-		if (tmp[ny][nx] == 1 || tmp[ny][nx] == 2)
+		if (ny < 0 || nx < 0 || ny >= n || nx >= m)
 			continue;
 
-		tmp[ny][nx] = 2;
+		if (board[ny][nx] == 1 || board[ny][nx] == 2)
+			continue;
+
+		board[ny][nx] = 2;
 		DFS(n, m, ny, nx);
 	}
 
 	return;
 }
 
-//최대 안전영역
-int get_Safe_size(int n, int m) {
+// 안전지대갯수
+int get_Safe_Area(int n, int m) {
 	int cnt = 0;
-	for (int i = 1; i <= n; ++i) {
-		for (int j = 1; j <= m; ++j) {
-			if (tmp[i][j] == 0)
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			if (board[i][j] == 0)
 				cnt++;
 		}
 	}
@@ -57,44 +39,53 @@ int get_Safe_size(int n, int m) {
 	return cnt;
 }
 
-void make_wall(int n, int m, int cnt) {
-	if (cnt == 3) {
-		//백업
-		backup(n, m);
+void make_Wall(int n, int m, int level) {
+	if (level == 3) {
+		int tmp_board[9][9];
+		// 복사
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < m; ++j) 
+				tmp_board[i][j] = board[i][j];
+		}
 
-		for (int i = 1; i <= n; ++i) {
-			for (int j = 1; j <= m; ++j) {
-				if (tmp[i][j] == 2)
+		// 바이러스 확산
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < m; ++j) {
+				if (board[i][j] == 2)
 					DFS(n, m, i, j);
 			}
 		}
-	
-		result = Max(result, get_Safe_size(n, m));
+		int tmp = get_Safe_Area(n, m);
+		if (result < tmp)
+			result = tmp;
+		// 복원
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < m; ++j)
+				board[i][j] = tmp_board[i][j];
+		}
+
 		return;
 	}
 
-	for (int i = 1; i <= n; ++i) {
-		for (int j = 1; j <= m; ++j) {
-			//벽을 발견 -> 백트래킹
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
 			if (board[i][j] == 0) {
-				cnt++;
 				board[i][j] = 1;
-				make_wall(n, m, cnt);
+				make_Wall(n, m, level + 1);
 				board[i][j] = 0;
-				cnt--;
 			}
 		}
 	}
 
 	return;
-
 }
 
 int solution(int n, int m) {
 	int answer = 0;
 
-	make_wall(n, m, 0);
+	make_Wall(n, m, 0);
 	answer = result;
+
 	return answer;
 }
 
@@ -102,14 +93,12 @@ int main(void) {
 	int n, m;
 
 	cin >> n >> m;
-
-	for (int i = 1; i <= n; ++i) {
-		for (int j = 1; j <= m; ++j)
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j)
 			cin >> board[i][j];
 	}
 
 	int ret = solution(n, m);
 	cout << ret;
-
 	return 0;
 }
