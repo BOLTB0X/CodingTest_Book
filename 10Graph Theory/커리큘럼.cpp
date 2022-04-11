526ae7b19738f20c7046088f7289144822e6e7ab
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-11 11:01:49 +0900

    커리큘럼

--- 10Graph Theory/커리큘럼.cpp
+++ 10Graph Theory/커리큘럼.cpp
@@ -15,17 +15,15 @@ int times[501];
 // 위상 정렬 함수 
 void topology_Sort() {
     vector<int> result(501); // 알고리즘 수행 결과를 담을 리스트 
-    for (int i = 1; i <= v; i++) {
+    for (int i = 1; i <= v; i++) 
         result[i] = times[i];
-    }
 
     queue<int> q; // 큐 라이브러리 사용
 
     // 처음 시작할 때는 진입차수가 0인 노드를 큐에 삽입
     for (int i = 1; i <= v; i++) {
-        if (indegree[i] == 0) {
+        if (indegree[i] == 0) 
             q.push(i);
-        }
     }
 
     // 큐가 빌 때까지 반복
@@ -36,18 +34,18 @@ void topology_Sort() {
         // 해당 원소와 연결된 노드들의 진입차수에서 1 빼기
         for (int i = 0; i < graph[now].size(); i++) {
             result[graph[now][i]] = max(result[graph[now][i]], result[now] + times[graph[now][i]]);
-            indegree[graph[now][i]] -= 1;
+            indegree[graph[now][i]]--;
             // 새롭게 진입차수가 0이 되는 노드를 큐에 삽입
-            if (indegree[graph[now][i]] == 0) {
+            if (indegree[graph[now][i]] == 0)
                 q.push(graph[now][i]);
-            }
         }
     }
 
     // 위상 정렬을 수행한 결과 출력
-    for (int i = 1; i <= v; i++) {
+    for (int i = 1; i <= v; i++) 
         cout << result[i] << '\n';
-    }
+
+    return;
 }
 
 int main(void) {
@@ -63,7 +61,7 @@ int main(void) {
         while (true) {
             cin >> x;
             if (x == -1) break;
-            indegree[i] += 1;
+            indegree[i]++;
             graph[x].push_back(i);
         }
     }
