53ab4906b9739af0bada99bda62e878626ff224e
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-12 08:52:15 +0900

    정확한 순위

--- 09Shortest Path_실전/정확한 순위.cpp
+++ 09Shortest Path_실전/정확한 순위.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#define INF 1e9
+#define INF 1e9 // 10억으로 무한대 설정
 
 using namespace std;
 
@@ -10,13 +10,17 @@ int Min(int a, int b) {
 	return a < b ? a : b;
 }
 
+// 초기화
 void init(int n) {
-	for (int i = 0; i < 501; ++i)
+	// 무한대로 초기화
+	for (int i = 0; i < 501; ++i) 
 		fill(adj[i], adj[i] + 501, INF);
-
+	
 	for (int i = 1; i <= n; ++i) {
-		for (int j = 1; j <= n; ++j)
-			if (i == j) adj[i][j] = 0;
+		for (int j = 1; j <= n; ++j) {
+			if (i == j)
+				adj[i][j] = 0;
+		}
 	}
 
 	return;
@@ -25,19 +29,23 @@ void init(int n) {
 int solution(int n, int m) {
 	int answer = 0;
 
+	// 플로이드
 	for (int k = 1; k <= n; ++k) {
 		for (int a = 1; a <= n; ++a) {
-			for (int b = 1; b <= n; ++b) 
+			for (int b = 1; b <= n; ++b)
 				adj[a][b] = Min(adj[a][b], adj[a][k] + adj[k][b]);
 		}
 	}
-
-	for (int a = 1; a <= n; ++a) {
+	
+	for (int i = 1; i <= n; ++i) {
 		int cnt = 0;
-		for (int b = 1; b <= n; ++b) {
-			if (adj[a][b] != INF || adj[b][a] != INF)
+		for (int j = 1; j <= n; ++j) {
+			// 양방향으로 무한대가 아니면
+			// 비교를 한 것
+			if (adj[i][j] != INF || adj[j][i] != INF)
 				cnt++;
 		}
+
 		if (cnt == n)
 			answer++;
 	}
